35d77027471d1193387572049f013c5b7aed75a9
ITLATTICE <zqu1016@qq.com>
2022-12-03 22:50:34 +0800

    微信Native支付

---
+++ src/service/wxpayService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace iboxs\payment\service;
+
+class wxpayService
+{
+
+}
--- test/index.php
+++ test/index.php
@@ -3,6 +3,6 @@ namespace iboxs\test;
 require __DIR__."/../vendor/autoload.php";
 use iboxs\payment\Payment;
 $config=require_once __DIR__."/config.php";
-$result=Payment::Client('alipay',$config['alipay'])->alipayCodePay('2022',10,'名称','10m');
+$result=Payment::Client('wxpay',$config['weixin'])->wxpayCodepay('2022',10,'名称','10m');
 var_dump($result);
 ?>
